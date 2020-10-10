66593dca5aec14562f6b9a02045a0ee718671826
DiviyanshaAgarwal <71273582+DiviyanshaAgarwal@users.noreply.github.com>
2020-10-10 16:48:18 +0530

    Add files via upload

---
+++ WebContent/JS/navigation.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+function navigateBack() {
+	location.replace("schemeManagement.jsp")
+}
+
+function displayScheme() {
+	location.replace("displaySchemes.jsp")
+}
+
+function applyScheme() {
+    location.replace("applyScheme.jsp")
+    }
+
+function switchPage() {
+    location.replace("schemeManagement.jsp")
+    }
+
+function navigateLogin() {
+    location.replace("citizenLogin.jsp")
+    }
+
+function addScheme() {
+    location.replace("addScheme.jsp")
+    }
+
+function applyScheme() {
+    location.replace("applyScheme.jsp")
+    }
+function redirectServlet(){
+	
+}
